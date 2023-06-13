b44eadb2e8afc6ecec06bf3da46745bb47cbe40f
Priya Risky Qurniawan <80609729+Priyasan181@users.noreply.github.com>
2023-06-13 18:03:19 +0700

    rivisi2

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -24,7 +24,7 @@ const Favorite = {
         RestaurantContainer.innerHTML += createRestaurantItemTemplate(Restaurants);
       });
     } else {
-      RestaurantContainer.innerHTML = `<div">Tidak ada restoran untuk ditampilkan</div>
+      RestaurantContainer.innerHTML = `<div">Tidak ada restaurant untuk ditampilkan</div>
     `;
     // Fungsi ini akan dipanggil setelah render()
     }
